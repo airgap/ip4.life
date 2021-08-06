a5acb3fd2e37d8da51c04b33b4e059d6dc2d0fee
Nicole Muzzin <nick.muzzin@gmail.com>
2021-08-06 02:07:42 +0000

    Documented build process

--- build.sh
+++ build.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+# Clean previous build
 ./clean.sh
+
+# Create dist and compiled directories
 mkdir dist
 mkdir compiled
+
+# Install NPM modules
 npm i
+
 cd src || exit
+
+# Compile TypeScript and Sass
 tsc
 sass index.sass ../compiled/index.css
+
 cd .. || exit
+
+# Inject the HTML and CSS into index.js
 ts-node inject.ts
+
+# Clean the temporary build artifacts
 ./clean.sh no-dist
